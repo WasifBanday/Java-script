9aeff1b857f0a18698c8c53129a29cbbdaade219
wasif_manzoor <wasifahmad679@gmail.com>
2025-08-22 22:42:22 +0530

    More on data-types

---
+++ Java-script/pizza.js
@@ -0,0 +1,21 @@
+let numberofGuest=0;
+let pizzasize;
+// small-->for 2 people  
+// medium -->for 3-5 people
+// large --> for more then 5
+
+if(numberofGuest<=0){
+    pizzasize="Hanji fir kyu hi karna hai order"
+    console.log(pizzasize)
+}
+else if(numberofGuest<=2){
+    pizzasize="small"
+    console.log(pizzasize)
+}
+else if (numberofGuest<=5){
+    pizzasize="medium"
+    console.log(pizzasize)
+}else {
+    pizzasize="large"
+    console.log(pizzasize)
+}
